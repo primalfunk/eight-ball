5e6e82e1f89fdff24615519f39a09c693bbcf078
Jared Menard <jared.d.menard@gmail.com>
2018-05-10 15:38:43 -0600

    added colors, need answers class

--- main.rb
+++ main.rb
@@ -1,7 +1,23 @@
 #Magic Eight Ball team project
-require "answers"
-require "colorize"
+require 'colorize'
+#require 'answers'
 
-puts "*" * 26
-puts "Magic Eight Ball Simulator"
-puts "*" * 26
+puts "\n\n"
+puts "*".colorize(:green) * 26
+puts "Magic Eight Ball Simulator".colorize(:light_green)
+puts "*".colorize(:green) * 26
+puts "\n\n"
+
+puts "a) Add an answer"
+puts "b) Print all available answers"
+puts "c) Reset answer base to defaults, or just"
+print "Ask a question of the All-knowing Eight Ball: "
+question = gets.strip
+
+#handle user input
+  #use the class methods from the answers file
+
+
+
+
+
