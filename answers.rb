7cddf3c0243dee34c7bee8676c596df54b2b63b4
Jen Broadhead <jeniffer.broadhead@gmail.com>
2018-05-10 16:07:11 -0600

    initial commit

--- answers.rb
+++ answers.rb
@@ -1,5 +1,28 @@
+<<<<<<< HEAD
 class answers
   def initialize
     
   end
 end
+=======
+class Answers
+   
+    def insults
+    [
+        "Please jump off a cliff", 
+        "Contrary to popular belief, smoking saves lives",
+        "It's really mediocre",
+        ""
+    ]
+    end
+
+    def hurl
+        puts insults.sample(1)
+    end
+end
+
+output = Answers.new
+
+output.hurl
+
+>>>>>>> initial commit
