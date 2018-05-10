de0bc13f8f2aeac9226b16528ec41da5fa0b0daa
Pete Sword <pete.sword@yahoo.com>
2018-05-10 15:42:49 -0600

    initial commit

--- answers.rb
+++ answers.rb
@@ -12,5 +12,24 @@ class Answers
     def hurl
         puts insults.sample(1)
     end
+
+
+  def nice_answers
+    [
+      "Yes",
+      "No",
+      "Ask again later",
+      "Pobably not smart",
+      "It is decidedly so",
+      "Ask again later"
+    ]
+  end
+
+  def random_answer
+    nice_answers[Random.rand(nice_answers.size)]
+    puts nice_answers.sample
+  end
+
+  
 end
 
