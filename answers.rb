9875627712121d21fc1d9d61820bb22fae70866c
Jared Menard <jared.d.menard@gmail.com>
2018-05-10 20:08:52 -0600

    working version with notes

--- answers.rb
+++ answers.rb
@@ -1,36 +1,35 @@
 class Answers
-  def nice_answers
-    [
-      "Yes",
-      "No",
-      "Ask again later",
-      "Pobably not smart",
-      "It is decidedly so",
-      "Ask again later"
-    ]
-    end
-
-    def hurl
-        puts insults.sample(1)
-    end
-
-
-  def nice_answers
-    [
-      "Yes",
-      "No",
-      "Ask again later",
-      "Pobably not smart",
-      "It is decidedly so",
-      "Ask again later"
-    ]
-  end
-
-  def self.random_answer
-    nice_answers[Random.rand(nice_answers.size)]
-    puts nice_answers.sample
-  end
-
+  attr_accessor :response_arr, :random_answer, :add_new_answer, :print_all, :reset_array
   
-end
+  def initialize(arr)
+    #When the class initializes, it sets this class instance variable to the argument; the original list of answers lives on main.rb.
+    #It also sets an instance variable for the counter @i, which tracks how many times the user has added a new answer to the array. 
+    #This is so we can "pop" off the right number in the reset_array function.
+    @answer_list = arr
+    @i = 0
+  end
+  
+  def random_answer
+    #in the class, the main list of answers is @answer_list. Doesn't matter what it is called in the main file.
+    puts @answer_list.sample
+  end
 
+  def add_new_answer(new_answer)
+    #pushes the argument variable onto @answerlist, increments the counter @i
+    @answer_list << new_answer
+    @i += 1
+    puts "\nAdded answer: #{new_answer}"
+  end
+
+  def print_all(arr)
+    puts "\n\n"
+    puts arr
+  end
+
+  def reset_array
+    #uses the 'answers added' counter @i to pop off the @answer list end, then resets the counter to 0.
+    @answer_list.pop(@i)
+    puts "\nAnswers reset to values at init."
+    @i = 0
+  end
+end
--- main.rb
+++ main.rb
@@ -1,32 +1,49 @@
 #Magic Eight Ball team project
 require 'colorize'
-#require 'answers'
+require_relative 'answers'
 
-puts "\n\n"
-puts "*".colorize(:green) * 26
-puts "Magic Eight Ball Simulator".colorize(:light_green)
-puts "*".colorize(:green) * 26
-puts "\n\n"
+#I shortened the response array purely for readability reasons. Add as many as you like.
+@response_array =
+[
+  "No",
+  "It is decidedly so.",
+  "Who cares?",
+  "Get lost, creep.",
+  "That may be the dumbest thing I have ever heard.",
+  "Hey, look who knows how to press buttons on a keyboard! Good job, buddy."
+]
 
-puts "a) Add an answer"
-puts "b) Print all available answers"
-puts "c) Reset answer base to defaults"
-puts "q) Quit the program, or just"
-print "Ask a question of the All-knowing Eight Ball: "
-question = gets.strip
+#instantiating the answer list object with the array as above. In the answers class, this becomes @answer_list
+answer_list = Answers.new(@response_array)
 
-#handle user input
-if question == 'a'
-  #use the answers class method to add another answer to the array
-elsif question == 'b'
-  #class method to print all answers
-elsif question == 'c'
-  #class method to reset answer base to defaults
-elsif question == 'q'
-  puts "No more Eight Ball for you."
-  exit
-else
-  answers.nice_answer
+while true
+  #drawing the pretty header/menu
+  puts "\n\n"
+  puts "*".colorize(:green) * 26
+  puts "Magic Eight Ball Simulator".colorize(:light_green)
+  puts "*".colorize(:green) * 26
+  puts "\n\n"
+  puts "a) Add an answer"
+  puts "b) Print all available answers"
+  puts "c) Reset answer base to defaults"
+  puts "q) Quit the program, or just"
+  print "Ask a question of the All-knowing Eight Ball: "
+  question = gets.strip
+  #handle user input
+  if question == 'a'
+    puts "\nPlease enter a new answer to add."
+    resp = gets.strip
+    answer_list.add_new_answer(resp)
+  elsif question == 'b'
+    answer_list.print_all(@response_array)
+  elsif question == 'c'
+    answer_list.reset_array
+  elsif question == 'q'
+    puts "\nNo more Eight Ball for you.".colorize("red")
+    exit
+  else
+    answer_list.random_answer
+  end
 end
 
 
